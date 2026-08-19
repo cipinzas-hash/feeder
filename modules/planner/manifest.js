ae04547097210715b90f53f2c039a026e621417f
cipinzas-hash <cipinzas@gmail.com>
2026-08-19 18:01:26 -0400

    refactor: formalize Planner domain entrypoints

--- modules/planner/manifest.js
+++ modules/planner/manifest.js
@@ -1,3 +1,15 @@
+import {
+  BASE_DATE,
+  DAY_NAMES,
+  DEFAULT_COOKING_OPTS,
+  DEFAULT_ASEO_OPTS,
+  CYNICAL_SUBTITLES,
+  STOIC_PHRASES,
+  makeEmptyDay,
+  isWithKids,
+  fmtTime,
+} from "./domain.js";
+
 export default {
   id: "planner",
   version: "0.0",
@@ -9,8 +21,17 @@ export default {
     routines: { default: [] },
     recurring: { default: [] },
     lastRollover: { default: null },
-    cookingOpts: { default: [] },
-    aseoOpts: { default: [] },
+    cookingOpts: { default: DEFAULT_COOKING_OPTS },
+    aseoOpts: { default: DEFAULT_ASEO_OPTS },
     custody: { default: { baseDate: null, withKids: true, overrides: {} } },
+  },
+  domain: {
+    BASE_DATE,
+    DAY_NAMES,
+    CYNICAL_SUBTITLES,
+    STOIC_PHRASES,
+    makeEmptyDay,
+    isWithKids,
+    fmtTime,
   },
 };
