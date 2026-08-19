b19fcf48bc1579dad92eb6e3efada1a7e2c2619b
cipinzas-hash <cipinzas@gmail.com>
2026-08-19 18:03:22 -0400

    architecture: expose Planner 0.0 public entrypoint

---
+++ modules/planner/index.js
@@ -0,0 +1,13 @@
+export { default as manifest } from "./manifest.js";
+
+export {
+  BASE_DATE,
+  DAY_NAMES,
+  DEFAULT_COOKING_OPTS,
+  DEFAULT_ASEO_OPTS,
+  CYNICAL_SUBTITLES,
+  STOIC_PHRASES,
+  makeEmptyDay,
+  isWithKids,
+  fmtTime,
+} from "./domain.js";
