bf834ffd303d44689b299b44902cbc34bf264fde
cipinzas-hash <cipinzas@gmail.com>
2026-08-19 17:57:30 -0400

    architecture: version Fadiman module as 0.0

--- modules/fadiman/manifest.js
+++ modules/fadiman/manifest.js
@@ -1,7 +1,10 @@
 export default {
   id: "fadiman",
+  version: "0.0",
   tabLabel: "Fadiman",
   state: {
     fadimanData: { default: {} },
   },
 };
+
+// State-contract audit remains pending for protocol logs used by the page.
