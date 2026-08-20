dd102c84c410d43a18d37b8b58ec608d37296400
cipinzas-hash <cipinzas@gmail.com>
2026-08-20 05:35:25 -0400

    feat: mount Planner schedule in day card

--- modules/planner/PlannerDayCard.jsx
+++ modules/planner/PlannerDayCard.jsx
@@ -4,9 +4,9 @@ import PlannerTaskList from "./PlannerTaskList.jsx";
 import PlannerDayUtilities from "./PlannerDayUtilities.jsx";
 import PlannerDayReflection from "./PlannerDayReflection.jsx";
 import PlannerShopping from "./PlannerShopping.jsx";
+import PlannerSchedule from "./PlannerSchedule.jsx";
 
 const DEFAULT_DAY = { tasks: [], abasto: "", cookingMode: "", aseoMode: "", menu: "", summary: "", humors: [], humorCustom: [], compras: [], schedule: [] };
-
 function normalizeDay(day) { return day || DEFAULT_DAY; }
 
 export default function PlannerDayCard({ state, dateKey, dayName, holidayLabel = "", isToday = false, isWeekend = false, cookingOptions = [], aseoOptions = [], actions = {}, integrations = {} }) {
@@ -18,6 +18,7 @@ export default function PlannerDayCard({ state, dateKey, dayName, holidayLabel =
     React.createElement("header", { className: "planner-day-card__header" }, React.createElement("strong", null, dayName), React.createElement("span", null, dateKey), holidayLabel ? React.createElement("small", null, holidayLabel) : null),
     React.createElement(PlannerTaskList, { tasks: day.tasks || [], actions: taskActions, integrations }),
     React.createElement(PlannerShopping, { items: day.compras || [], actions: { updateItems: (items) => updateDay({ compras: items }) } }),
+    React.createElement(PlannerSchedule, { schedule: day.schedule || [], actions: { updateSchedule: (schedule) => updateDay({ schedule }) } }),
     React.createElement(PlannerDayUtilities, { day, cookingOptions, aseoOptions, actions: { updateDay }, integrations }),
     React.createElement(PlannerDayReflection, { day, actions: { updateDay }, integrations }),
     React.createElement("footer", null, isWeekend ? "fin de semana" : "día laboral"),
